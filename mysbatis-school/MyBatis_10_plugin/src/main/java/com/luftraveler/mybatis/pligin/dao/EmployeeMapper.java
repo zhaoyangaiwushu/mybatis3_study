800bf6227667b05f2e39e626465b26e47e91b6a8
admin <462321010@qq.com>
2020-09-18 20:01:57 +0800

    jkgc

---
+++ mysbatis-school/MyBatis_10_plugin/src/main/java/com/luftraveler/mybatis/pligin/dao/EmployeeMapper.java
@@ -0,0 +1,10 @@
+package com.luftraveler.mybatis.pligin.dao;
+
+
+import com.luftraveler.mybatis.pligin.bean.Employee;
+
+public interface EmployeeMapper {
+
+    public Employee getEmpById(Integer id);
+
+}
---
+++ mysbatis-school/MyBatis_10_plugin/src/main/java/com/luftraveler/mybatis/pligin/test/MyBatisTest.java
@@ -0,0 +1,41 @@
+package com.luftraveler.mybatis.pligin.test;
+
+import com.luftraveler.mybatis.pligin.bean.Employee;
+import com.luftraveler.mybatis.pligin.dao.EmployeeMapper;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MyBatisTest {
+
+
+	public SqlSessionFactory getSqlSessionFactory() throws IOException {
+		String resource = "mybatis-config.xml";
+		InputStream inputStream = Resources.getResourceAsStream(resource);
+		return new SqlSessionFactoryBuilder().build(inputStream);
+	}
+
+	@Test
+	public void test01() throws IOException {
+		// 1、获取sqlSessionFactory对象
+		SqlSessionFactory sqlSessionFactory = getSqlSessionFactory();
+		// 2、获取sqlSession对象
+		SqlSession openSession = sqlSessionFactory.openSession();
+		try {
+			// 3、获取接口的实现类对象
+			//会为接口自动的创建一个代理对象，代理对象去执行增删改查方法
+			EmployeeMapper mapper = openSession.getMapper(EmployeeMapper.class);
+			Employee employee = mapper.getEmpById(1);
+			System.out.println(mapper.getClass());
+			System.out.println(employee);
+		} finally {
+			openSession.close();
+		}
+	}
+
+}
